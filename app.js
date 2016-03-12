abd326fb4dbca21beda2631c3d0b6914cd02e7be
ekashivagui <mushira4@gmail.com>
2016-03-11 21:29:18 -0300

    Adding bootstrap and templates

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // Loadig modules
 var express = require('express');
 var load = require('express-load');
-var app = express();
+var app = express(); // The express application
 
 // View engine setup
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+//__dirname = current app directory path
+
 app.use(express.static(__dirname + '/public'));
+app.use('/bootstrap/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 load('models')
   .then('controllers')
@@ -33,17 +36,17 @@ if (app.get('env') === 'development') {
       error: err
     });
   });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+} else {
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
   });
-});
+}
 
 app.listen(3000, function(){
   console.log("Ntalk up and running.");
